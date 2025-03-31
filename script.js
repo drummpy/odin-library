9bb2b888530072d679933c7197c3fdb93bfc59a7
drummpy <154277179+drummpy@users.noreply.github.com>
2025-03-30 17:45:43 -0700

    Add loop through books and display on page

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const bookshelf = document.querySelector(".right");
 
 function Book(author, title, pages, read) {
   this.author = author;
@@ -9,14 +10,27 @@ function Book(author, title, pages, read) {
 }
 
 function addBookToLibrary() {
-  newBook = new Book("Dickens", "The Night", 633, true);
+  newBook = new Book("Dickens", "The Night", 633, "Yes");
 
   myLibrary.push(newBook);
 
-  newBook = new Book("Johnson", "Where's my Johnson?", 12, false);
+  newBook = new Book("Johnson", "Where's my Johnson?", 12, "No");
 
   myLibrary.push(newBook);
 }
 
 addBookToLibrary();
 console.log(myLibrary);
+
+myLibrary.forEach((book) => {
+  const bookdiv = document.createElement("div");
+  bookdiv.classList.add("bookfield");
+  bookdiv.innerHTML = `
+  <p><b>Author:</b> ${book.author}</p>
+  <p><b>Title:</b> ${book.title}</p>
+  <b>Pages:</b> ${book.pages}</p>
+  <b>Read?</b> ${book.read}</p>`;
+  bookdiv.id = book.id;
+
+  bookshelf.appendChild(bookdiv);
+});
