4aca9901ec5470fca04a844f5e25d97240728fa2
drummpy <154277179+drummpy@users.noreply.github.com>
2025-04-02 12:29:01 -0700

    Add adding book function with dialog

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const myLibrary = [];
 const bookshelf = document.querySelector(".right");
+const openDiag = document.getElementById("opendiag");
+const closeDiag = document.getElementById("closediag");
+const addBookBtn = document.getElementById("addbookdiag");
+const form = document.querySelector("form");
+
+const authorInput = document.getElementById("author");
+const titleInput = document.getElementById("title");
+const pagesInput = document.getElementById("pages");
+const readInput = document.getElementById("read");
 
 function Book(author, title, pages, read) {
   this.author = author;
@@ -9,28 +18,52 @@ function Book(author, title, pages, read) {
   this.id = self.crypto.randomUUID();
 }
 
-function addBookToLibrary() {
-  newBook = new Book("Dickens", "The Night", 633, "Yes");
-
-  myLibrary.push(newBook);
-
-  newBook = new Book("Johnson", "Where's my Johnson?", 12, "No");
-
+function addBookToLibrary(author, title, pages, read) {
+  newBook = new Book(author, title, pages, read);
   myLibrary.push(newBook);
 }
 
-addBookToLibrary();
-console.log(myLibrary);
+function showBooks() {
+  bookshelf.innerHTML = ""; //Clear Bookshelf
 
-myLibrary.forEach((book) => {
-  const bookdiv = document.createElement("div");
-  bookdiv.classList.add("bookfield");
-  bookdiv.innerHTML = `
-  <p><b>Author:</b> ${book.author}</p>
-  <p><b>Title:</b> ${book.title}</p>
-  <b>Pages:</b> ${book.pages}</p>
-  <b>Read?</b> ${book.read}</p>`;
-  bookdiv.id = book.id;
+  myLibrary.forEach((book) => {
+    const bookdiv = document.createElement("div");
+    bookdiv.classList.add("bookfield");
+    bookdiv.innerHTML = `
+    <p><b>Author:</b> ${book.author}</p>
+    <p><b>Title:</b> ${book.title}</p>
+    <p><b>Pages:</b> ${book.pages}</p>
+    <p><b>Read?</b> ${book.read}</p>`;
+    bookdiv.id = book.id;
 
-  bookshelf.appendChild(bookdiv);
+    bookshelf.appendChild(bookdiv);
+  });
+}
+
+openDiag.addEventListener("click", () => {
+  dialog.showModal();
 });
+
+closeDiag.addEventListener("click", (event) => {
+  event.preventDefault();
+  dialog.close();
+  // if (form.checkValidity()) {
+  //   dialog.close();
+  // }
+});
+
+addBookBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  const author = authorInput.value;
+  const title = titleInput.value;
+  const pages = pagesInput.value;
+  const read = readInput.checked ? "Yes" : "No";
+  addBookToLibrary(author, title, pages, read);
+  showBooks();
+  dialog.close();
+  form.reset();
+});
+
+addBookToLibrary("Dickens", "The Night", 633, "Yes");
+addBookToLibrary("Johnson", "Where's my Johnson?", 12, "No");
+console.log(myLibrary);
