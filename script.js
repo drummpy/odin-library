96548f238aad4395ebeef70a3745f210a61633b1
drummpy <154277179+drummpy@users.noreply.github.com>
2025-04-24 16:23:01 -0700

    Refactored into class structure

--- script.js
+++ script.js
@@ -1,5 +1,71 @@
-let myLibrary = [];
-const bookshelf = document.querySelector(".right");
+class Book {
+  constructor(author, title, pages, read) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+    this.id = self.crypto.randomUUID();
+  }
+}
+
+class Library {
+  constructor() {
+    this.books = [];
+    this.bookshelf = document.querySelector(".right");
+  }
+
+  addBookToLibrary(author, title, pages, read) {
+    const newBook = new Book(author, title, pages, read);
+    this.books.push(newBook);
+    this.showBooks();
+  }
+
+  removeBookFromLibrary(id) {
+    this.books = this.books.filter((book) => book.id !== id);
+    this.showBooks();
+  }
+
+  swapReadStatus(id) {
+    const book = this.books.find((book) => book.id === id);
+
+    if (book) {
+      book.read = book.read === "Yes" ? "No" : "Yes";
+    }
+    this.showBooks();
+  }
+
+  showBooks() {
+    this.bookshelf.innerHTML = ""; //Clear Bookshelf
+
+    this.books.forEach((book) => {
+      const bookdiv = document.createElement("div");
+      bookdiv.classList.add("bookfield");
+      bookdiv.innerHTML = `
+    <p><b>Author:</b> ${book.author}</p>
+    <p><b>Title:</b> ${book.title}</p>
+    <p><b>Pages:</b> ${book.pages}</p>
+    <p><b>Read?</b> ${book.read}</p>
+    <button class="deletebtn" id="delete-${book.id}">Delete</button>
+    <button class="readbtn" id="read-${book.id}">Read?</button>`;
+      bookdiv.id = book.id;
+
+      this.bookshelf.appendChild(bookdiv);
+
+      const deleteButton = document.getElementById("delete-" + book.id);
+      deleteButton.addEventListener("click", () => {
+        this.removeBookFromLibrary(book.id);
+      });
+      const readButton = document.getElementById("read-" + book.id);
+      readButton.addEventListener("click", () => {
+        this.swapReadStatus(book.id);
+      });
+    });
+  }
+}
+
+const library = new Library();
+
+const dialog = document.getElementById("dialog");
 const openDiag = document.getElementById("opendiag");
 const closeDiag = document.getElementById("closediag");
 const addBookBtn = document.getElementById("addbookdiag");
@@ -10,62 +76,6 @@ const titleInput = document.getElementById("title");
 const pagesInput = document.getElementById("pages");
 const readInput = document.getElementById("read");
 
-function Book(author, title, pages, read) {
-  this.author = author;
-  this.title = title;
-  this.pages = pages;
-  this.read = read;
-  this.id = self.crypto.randomUUID();
-}
-
-function addBookToLibrary(author, title, pages, read) {
-  newBook = new Book(author, title, pages, read);
-  myLibrary.push(newBook);
-}
-
-function removeBookFromLibrary(id) {
-  myLibrary = myLibrary.filter((book) => book.id !== id);
-  showBooks();
-}
-
-function swapReadStatus(id) {
-  const book = myLibrary.find((book) => book.id === id);
-
-  if (book) {
-    book.read = book.read === "Yes" ? "No" : "Yes";
-  }
-  showBooks();
-}
-
-function showBooks() {
-  bookshelf.innerHTML = ""; //Clear Bookshelf
-
-  myLibrary.forEach((book) => {
-    const bookdiv = document.createElement("div");
-    bookdiv.classList.add("bookfield");
-    bookdiv.innerHTML = `
-    <p><b>Author:</b> ${book.author}</p>
-    <p><b>Title:</b> ${book.title}</p>
-    <p><b>Pages:</b> ${book.pages}</p>
-    <p><b>Read?</b> ${book.read}</p>
-    <button class="deletebtn" id="delete-${book.id}">Delete</button>
-    <button class="readbtn" id="read-${book.id}">Read?</button>`;
-    bookdiv.id = book.id;
-
-    bookshelf.appendChild(bookdiv);
-
-    const deleteButton = document.getElementById("delete-" + book.id);
-    deleteButton.addEventListener("click", () => {
-      removeBookFromLibrary(book.id);
-    });
-    const readButton = document.getElementById("read-" + book.id);
-    readButton.addEventListener("click", () => {
-      swapReadStatus(book.id);
-    });
-    console.log(myLibrary);
-  });
-}
-
 openDiag.addEventListener("click", () => {
   dialog.showModal();
 });
@@ -73,21 +83,18 @@ openDiag.addEventListener("click", () => {
 closeDiag.addEventListener("click", (event) => {
   event.preventDefault();
   dialog.close();
-  // if (form.checkValidity()) {
-  //   dialog.close();
-  // }
 });
 
 addBookBtn.addEventListener("click", (event) => {
   event.preventDefault();
-  const author = authorInput.value;
-  const title = titleInput.value;
-  const pages = pagesInput.value;
-  const read = readInput.checked ? "Yes" : "No";
-  addBookToLibrary(author, title, pages, read);
-  showBooks();
+  library.addBookToLibrary(
+    authorInput.value,
+    titleInput.value,
+    pagesInput.value,
+    readInput.checked ? "Yes" : "No"
+  );
   dialog.close();
   form.reset();
 });
 
-showBooks();
+library.showBooks();
